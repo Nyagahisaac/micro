2c412a3d3defe3604b4bd6fb67489e07b4d71281
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-09-22 05:40:41 +0300

    add the password property and function

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ class User(UserMixin,db.Model):
     __tablename___ = 'users'
 
     
+
+    
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
